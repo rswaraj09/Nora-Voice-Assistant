f128e27a13d3732f9a92d93690efb4847fe1247b
Ritik Swaraj <swarajritik@gmail.com>
2026-02-13 18:34:41 +0530

    frontend

---
+++ templates/main.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+
+    eel.init()()
+
+    $('.text').textillate({
+        loop: true,
+        sync: true,
+        in: {
+            effect: "bounceIn",
+        },
+        out: {
+            effect: "bounceOut",
+        },
+
+    });
+});
